83c4a8955d25d9a0d9904c3f745e68c632eb90bd
DurganareshRasabattula <67402254+DurganareshRasabattula@users.noreply.github.com>
2021-08-15 12:32:47 +0530

    CoreJava-Security project added

---
+++ Parent-Modules/CoreJava-Modules/CoreJava-Security/src/main/com.durga/jaas/ResourcePermission.java
@@ -0,0 +1,9 @@
+package jaas;
+
+import java.security.BasicPermission;
+
+public class ResourcePermission extends BasicPermission {
+    public ResourcePermission(String name) {
+        super(name);
+    }
+}
