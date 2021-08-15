c0bbbf7ff814afe25c031fb1aabf582080ce6343
DurganareshRasabattula <67402254+DurganareshRasabattula@users.noreply.github.com>
2021-08-15 11:20:17 +0530

    CoreJava-String project added

---
+++ Parent-Modules/CoreJava-Modules/CoreJava-String-Conversions/src/main/com.durga/emptystrings/EmptyStringCheck.java
@@ -0,0 +1,8 @@
+package emptystrings;
+
+class EmptyStringCheck {
+
+    boolean isEmptyString(String string) {
+        return string == null || string.isEmpty();
+    }
+}
---
+++ Parent-Modules/CoreJava-Modules/CoreJava-String-Conversions/src/main/com.durga/emptystrings/Java5EmptyStringCheck.java
@@ -0,0 +1,8 @@
+package emptystrings;
+
+class Java5EmptyStringCheck {
+
+    boolean isEmptyString(String string) {
+        return string == null || string.length() == 0;
+    }
+}
